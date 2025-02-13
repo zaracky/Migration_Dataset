1ae93f360ba1314b6599f56f069391962e1d8d87
zaracky <51820857+zaracky@users.noreply.github.com>
2025-02-13 21:00:12 +0100

    Suppression valeurs par défaut

--- init-mongo.js
+++ init-mongo.js
@@ -4,8 +4,8 @@ db = db.getSiblingDB(process.env.DATABASE_NAME || "entreprise");
 
 // Création de l'utilisateur "consultant" avec accès en lecture
 db.createUser({
-  user: process.env.CONSULTANT_USERNAME || "consultant",
-  pwd: process.env.CONSULTANT_PASSWORD || "consultantpassword",
+  user: process.env.CONSULTANT_USERNAME,
+  pwd: process.env.CONSULTANT_PASSWORD,
   roles: [
     {
       role: "read",
@@ -16,8 +16,8 @@ db.createUser({
 
 // Création de l'utilisateur "devs" avec accès en lecture/écriture
 db.createUser({
-  user: process.env.DEVS_USERNAME || "devs",
-  pwd: process.env.DEVS_PASSWORD || "devspassword",
+  user: process.env.DEVS_USERNAME,
+  pwd: process.env.DEVS_PASSWORD,
   roles: [
     {
       role: "readWrite",
@@ -28,8 +28,8 @@ db.createUser({
 
 // Création de l'utilisateur "admin" avec accès complet
 db.createUser({
-  user: process.env.ADMIN_USERNAME || "adminuser",
-  pwd: process.env.ADMIN_PASSWORD || "adminuserpassword",
+  user: process.env.ADMIN_USERNAME ,
+  pwd: process.env.ADMIN_PASSWORD ,
   roles: [
     {
       role: "dbOwner",
